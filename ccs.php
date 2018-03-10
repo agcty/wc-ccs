cd90c60273f49611163359a85197df4d99bc5ff2
Alex Gogl <gogl.alex@gmail.com>
2018-03-10 15:31:54 +0100

    beatuify code

--- ccs.php
+++ ccs.php
@@ -12,76 +12,72 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
 
 if (!defined('ABSPATH')) {
-	exit;
+    exit;
 }
 
 /**
  * check if woocommerce is active
  */
 if (in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
-	function show_cross_sell_in_single_product()
-	{
-		/**
-		 * get the cross_sell_ids of the current viewn product
-		 */
-		global $product;
-		$crosssells = $product->get_cross_sell_ids();
-		/**
-		 * check if crosssells is empty, if there was no check WP_Query would still provide results
-		 * and would show you random products as cross sells
-		 */
-		if (empty($crosssells)) {
-			return;
-		}
+    function show_cross_sell_in_single_product()
+    {
+        /**
+         * get the cross_sell_ids of the current viewn product
+         */
+        global $product;
+        $crosssells = $product->get_cross_sell_ids();
+        /**
+         * check if crosssells is empty, if there was no check WP_Query would still provide results
+         * and would show you random products as cross sells
+         */
+        if (empty($crosssells)) {
+            return;
+        }
 
-		$args = array(
-			'post_type' => 'product',
-			/**
-			 * change to -1 if all cross sells of this product should be shown
-			 * -1 is not recommended for shopkeeper theme unless you have exactly
-			 * four cross sells for a product
-			 */
-			'posts_per_page' => 4,
-			'post__in' => $crosssells,
-			/**
-			 * If you're not paginating, this skips MYSQL_CALC statments
-			 */
-			'no_found_rows' => true,
-			/**
-			 * Skips updating meta cache
-			 */
-			'update_post_meta_cache' => false,
-			/**
-			 * Skips updating term cache.
-			 */
-			'update_post_term_cache' => false
-		);
+        $args = array(
+            'post_type' => 'product',
+            /**
+             * change to -1 if all cross sells of this product should be shown
+             * -1 is not recommended for shopkeeper theme unless you have exactly
+             * four cross sells for a product
+             */
+            'posts_per_page' => 4,
+            'post__in' => $crosssells,
+            /**
+             * If you're not paginating, this skips MYSQL_CALC statments
+             */
+            'no_found_rows' => true,
+            /**
+             * Skips updating meta cache
+             */
+            'update_post_meta_cache' => false,
+            /**
+             * Skips updating term cache.
+             */
+            'update_post_term_cache' => false
+        );
 
-		$products = new WP_Query($args);
+        $products = new WP_Query($args);
 
-		/**
-		 * check if product has posts, otherwise always true
-		 */
-		if ($products->have_posts()): ?>
+        /**
+         * check if product has posts, otherwise always true
+         */
+        if ($products->have_posts()): ?>
+      		<section class="related products">
+        		<h2>
+							<?php esc_html_e('Passend dazu', 'woocommerce'); ?>
+						</h2>
 
-        		<section class="related products">
+						<?php woocommerce_product_loop_start(); ?>
+							<?php while ($products->have_posts()): $products->the_post();
+	      	  		wc_get_template_part('content', 'product');
+	        		endwhile; ?>
+						<?php woocommerce_product_loop_end(); ?>
+					</section>
+				<?php endif;
+				
+        wp_reset_postdata();
+    }
 
-          		<h2><?php esc_html_e('Passend dazu', 'woocommerce'); ?></h2>
-
-			<?php woocommerce_product_loop_start(); ?>
-
-          		<?php while ($products->have_posts()): $products->the_post();
-				wc_get_template_part('content', 'product');
-			endwhile; ?>
-
-          		<?php woocommerce_product_loop_end(); ?>
-
-        		</section>
-
-      		<?php endif;
-
-		wp_reset_postdata();
-	}
-
-	add_action('woocommerce_after_single_product_summary', 'show_cross_sell_in_single_product', 10, 2);
+    add_action('woocommerce_after_single_product_summary', 'show_cross_sell_in_single_product', 10, 2);
 }
